dd31b3657252b44f1d69fe56272fa578dca94b11
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-29 18:34:04 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,18 +2,18 @@ def reverse_each_word (str)
   arr = str.split(" ")
   reversed_elements = []
 
-    arr.each do |wrd|
+  arr.each do |wrd|
     reversed_elements << wrd.reverse
-    end
-    return reversed_elements.join(' ')
+  end
+  return reversed_elements.join(' ')
 end
 
 def reverse_each_word (str)
   arr = str.split(" ")
   reversed_elements = []
 
-    arr.collect do |wrd|
+  arr.collect do |wrd|
     reversed_elements << wrd.reverse
-    end
+  end
   return reversed_elements.join(' ')
 end
