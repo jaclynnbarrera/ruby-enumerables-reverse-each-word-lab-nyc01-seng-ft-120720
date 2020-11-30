e1ad3fe2cdf7657dd027d4da56e08dd89f29e3c7
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-30 20:16:34 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,12 +8,6 @@ def reverse_each_word (str)
   return reversed_elements.join(' ')
 end
 
-def reverse_each_word (str)
-  arr = str.split(" ")
-  reversed_elements = []
-
-  arr.collect do |wrd|
-    reversed_elements << wrd.reverse
-  end
-  return reversed_elements.join(' ')
+def reverse_each_word(string)
+return string.split().collect{|element|element.reverse}.join(" ")
 end
